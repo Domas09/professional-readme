661324f3718524b81d1e15f507bb6ed63b12b386
Expired Milk <domasdar2@gmail.com>
2024-03-22 17:35:57 -0500

    Inquirer now has full list of questions and everything prints to README.md

--- generate/README.md
+++ generate/README.md
@@ -1,55 +1,57 @@
-# Frank
+# there
 
-  ## Description
+## Description
   
+once
   
+- was
+- a
+- sailor
+- who
   
-  - 
-  - 
-  - 
-  - 
+## Table of Contents
   
-  ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
   
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Credits](#credits)
-  - [License](#license)
+## Installation
   
-  ## Installation
+learned
   
+## Usage
   
+how
   
-  ## Usage
+## Credits
   
+to
   
+fish
   
-  ## Credits
+and
   
+## License
   
+  undefinedundefined
   
+---
   
+## How to Contribute
   
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
   
+## Tests
   
-  
-  
-  ---
-  
-  ## How to Contribute
-  
-  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-  
-  ## Tests
-  
-  Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 
-  ## Screenshot
+## Screenshot
 
-  
+was 
 
-  ## Questions
+## Questions
 
-  
-  
+cool
+yeah
 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('../professional-readme/utils/generateMarkdown');
 // TODO: Create an array of questions for user input
+const questions = ["Project title", "Brief description of project", "What was your motivation", "Why did you build this project", "What problem does it solve?", "What did you learn?", "Installation directions", "Usage instructions", "Collaborators", "Third party assests", "Links to tutorials", "License", "Screenshot directory", "Github username", "Email"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -40,9 +41,93 @@ function init() {
             name: 'title',
             message: questions[0]
         },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[1]
+        },
+        {
+            type: 'input',
+            name: 'motivation',
+            message: questions[2]
+        },
+        {
+            type: 'input',
+            name: 'why',
+            message: questions[3]
+        },
+        {
+            type: 'input',
+            name: 'problem',
+            message: questions[4]
+        },
+        {
+            type: 'input',
+            name: 'learn',
+            message: questions[5]
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[6]
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: questions[7]
+        },
+        {
+            type: 'input',
+            name: 'collab',
+            message: questions[8]
+        },
+        {
+            type: 'input',
+            name: 'third',
+            message: questions[9]
+        },
+        {
+            type: 'input',
+            name: 'tutorials',
+            message: questions[10]
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: questions[11]
+        },
+        {
+            type: 'input',
+            name: 'screenshot',
+            message: questions[12]
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: questions[13]
+        },
+        {
+            type: 'input',
+            name: 'gmail',
+            message: questions[14]
+        },
     ])
     .then((response) => {
         data.title = response.title;
+        data.description = response.description;
+        data.motivation = response.motivation;
+        data.why = response.why;
+        data.problem = response.problem;
+        data.learn = response.learn;
+        data.installation = response.installation;
+        data.instructions = response.instructions;
+        data.collab = response.collab;
+        data.third = response.third;
+        data.tutorials = response.tutorials;
+        data.license = response.license;
+        data.screenshot = response.screenshot;
+        data.github = response.github;
+        data.gmail = response.gmail;
         writeToFile("README.md", data);
     })
     
@@ -50,6 +135,5 @@ function init() {
     
 }
 
-const questions = ["Project title", "Brief description of project", "What was your motivation", "Why did you build this project", "What problem does it solve?", "What did you learn?", "Installation directions", "Usage instructions", "Collaborators", "Third party assests", "Links to tutorials", "License", "Screenshot directory", "Github username", "Email"];
 // Function call to initialize app
 init();
