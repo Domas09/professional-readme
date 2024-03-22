737c3c0dd16361366de590c9850b96a4ef51fa6f
Expired Milk <domasdar2@gmail.com>
2024-03-22 16:19:39 -0500

    Added all questions to array made template for README.md and linked all required packages

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('requirer');
+const generateMarkdown = require('../professional-readme/utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Project name", "Brief description of project", "What was your motivation", "Why did you build this project", "What problem does it solve?", "What did you learn?", "Instalation directions", "Directory for screenshot", "Collaborators", "Third party assests", "Links to tutorials", "License", "Github username", "Email"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(`../professional-readme/generate/${fileName}`, generateMarkdown(data), (err) => 
+    err ? console.error(err) : console.log("Yippee!")
+    );
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
