b1ea391ef191829aef2ae46fd3cb82ee05a89d2d
Expired Milk <domasdar2@gmail.com>
2024-03-22 17:17:36 -0500

    set up inquirer

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('../professional-readme/utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = ["Project title", "Brief description of project", "What was your motivation", "Why did you build this project", "What problem does it solve?", "What did you learn?", "Installation directions", "Usage instructions", "Collaborators", "Third party assests", "Links to tutorials", "License", "Screenshot directory", "Github username", "Email"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    
     fs.writeFile(`../professional-readme/generate/${fileName}`, generateMarkdown(data), (err) => 
     err ? console.error(err) : console.log("Yippee!")
     );
-
+    
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    const data = {
+    let data = {
         title: "",
         description: "",
         motivation: "",
@@ -34,9 +33,23 @@ function init() {
         gmail: ""
     }
 
-
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0]
+        },
+    ])
+    .then((response) => {
+        data.title = response.title;
+    })
+    
+    
+    
     writeToFile("README.md", data);
 }
 
+const questions = ["Project title", "Brief description of project", "What was your motivation", "Why did you build this project", "What problem does it solve?", "What did you learn?", "Installation directions", "Usage instructions", "Collaborators", "Third party assests", "Links to tutorials", "License", "Screenshot directory", "Github username", "Email"];
 // Function call to initialize app
 init();
